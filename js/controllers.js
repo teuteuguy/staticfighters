a022b5adfeba51394fbdb6d3a1b679ef55a34371
teuteuguy <pub@lalatte.com>
2014-03-17 17:11:11 +0800

    Angular JS Rocks bitches

--- js/controllers.js
+++ js/controllers.js
@@ -12,10 +12,27 @@ myAppControllers.controller('MyCtrl2', [function() {
 
   }]);
 
-myAppControllers.controller('ShuListController', ['$scope', '$http', function($scope, $http) {
-  	$http.get('http://api.shubacca.com/shu?consumer_key=4a8e628392a504eb746c37e1b0044f0f&sort=id,desc').success(function(data) {
-    	$scope.shus = data;
+myAppControllers.controller('ShuListController', function($scope, shubacca) { // ['$scope', '$http', function($scope, $http) {
+  	//$http.get('http://api.shubacca.com/shu?consumer_key=4a8e628392a504eb746c37e1b0044f0f&sort=id,desc').success(function(data) {
+	shubacca.getAllSHUs().success(function(shus) {
+
+  		shus.forEach(function(shu) {  			
+
+  			shubacca.getSHULastStatus(shu.id).success(function(status) {
+
+  				shu.status = status[0];
+
+  			});
+
+  			shubacca.getSHULastConfig(shu.id).success(function(config) {
+
+  				shu.config = config[0];
+
+  			});
+  		});
+
+    	$scope.shus = shus;
     });
 
     $scope.orderProp = 'description';
-}]);
+});//]);
--- js/services.js
+++ js/services.js
@@ -9,6 +9,35 @@ var myAppServices = angular.module('myApp.services', []);
 
 myAppServices.value('version', '0.1');
 
+myAppServices.value('utils', function() {
+
+});
+
+myAppServices.factory('shubacca', function($http) {
+
+	return {
+		getAllSHUs: function() {
+			
+			var url = "http://api.shubacca.com/shu?consumer_key=4a8e628392a504eb746c37e1b0044f0f&sort=id,desc";
+			
+			return $http.get(url);
+
+		}, 
+		getSHULastStatus: function( $id ) {
+
+			var url = "http://api.shubacca.com/shu/" + $id + "/status?consumer_key=4a8e628392a504eb746c37e1b0044f0f&sort=id,desc&limit=1";
+
+			return $http.get(url);
+		},
+		getSHULastConfig: function( $id ) {
+
+			var url = "http://api.shubacca.com/shu/" + $id + "/config?consumer_key=4a8e628392a504eb746c37e1b0044f0f&sort=id,desc&limit=1";
+
+			return $http.get(url);
+		}
+
+	};
+});
 // myAppServices.factory('Search',function(){
 // 	console.log
 //   return {text:''};
