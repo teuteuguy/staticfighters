d8d9b012e339e769334a1a97fd4cbbe2afc0b563
teuteuguy <pub@lalatte.com>
2014-03-19 16:57:43 +0800

    Changed version to 0.2

--- js/services.js
+++ js/services.js
@@ -7,7 +7,7 @@
 // In this case it is a simple value service.
 var myAppServices = angular.module('myApp.services', []);
 
-myAppServices.value('version', '0.1');
+myAppServices.value('version', '0.2');
 
 myAppServices.value('utils', function() {
 
