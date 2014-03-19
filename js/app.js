152743885ac8697d97d0b559b5c550bcb0abb52b
teuteuguy <pub@lalatte.com>
2014-03-20 00:33:02 +0800

    defaulting to shus

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ myApp.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/shus', {templateUrl: 'partials/shus.html', controller: 'controllerShuList'});
   //$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/map'});
+  $routeProvider.otherwise({redirectTo: '/shus'});
 
   //$locationProvider.html5Mode(true);
 });
